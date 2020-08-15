c75d01bb4d27c6a0993aa44fb8d599522f27194f
keiya shimomura <skeiya@gmail.com>
2020-08-15 16:47:55 +0900

    CEFPlacementerをコンソールアプリに変更。

--- CEFPlacementer/Program.cs
+++ CEFPlacementer/Program.cs
@@ -14,9 +14,6 @@ namespace CEFPlacementer
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }
