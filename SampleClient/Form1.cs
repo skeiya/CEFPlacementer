2c1a9cebaf3cd22277ce5ac6871d12bb2ebab7d2
keiya shimomura <skeiya@gmail.com>
2020-08-16 17:13:26 +0900

    位置指定の方法を変更。

--- SampleClient/Form1.cs
+++ SampleClient/Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace SampleClient
@@ -13,17 +14,24 @@ namespace SampleClient
 
         private void button1_Click(object sender, EventArgs e)
         {
-            Process.Start("CEFPlacementer.exe", "-lgoogle.com -p100:100:400:200");
+            Process.Start("CEFPlacementer.exe", "-lgoogle.com " + GetPositionArg());
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            Process.Start("CEFPlacementer.exe", "-lYahoo.co.jp -p100:100:200:400");
+            Process.Start("CEFPlacementer.exe", "-lYahoo.co.jp " + GetPositionArg());
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             Process.Start("CEFPlacementer.exe", "-c");
+        }
+
+        private string GetPositionArg()
+        {
+            var clientRect = new Rectangle(panel1.Location.X, panel1.Location.Y, panel1.Width, panel1.Height);
+            var screenRect = this.RectangleToScreen(clientRect);
+            return "-p" + screenRect.X.ToString() + ":" + screenRect.Y.ToString() + ":" + screenRect.Width.ToString() + ":" + screenRect.Height.ToString();
         }
     }
 }
