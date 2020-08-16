e2a0e7a0a963d5abd6413b64208defc925c655ce
keiya shimomura <skeiya@gmail.com>
2020-08-16 17:19:22 +0900

    タイトルバーを非表示。

--- CEFBrowser/Form1.cs
+++ CEFBrowser/Form1.cs
@@ -1,19 +1,26 @@
-﻿using System;
+﻿using CefSharp.WinForms;
+using System;
 using System.Windows.Forms;
 
 namespace CEFBrowser
 {
     public partial class Form1 : Form
     {
+        private ChromiumWebBrowser _chromiumWebBrowser1;
+
         public Form1()
         {
             InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            _chromiumWebBrowser1 = new ChromiumWebBrowser();
+            _chromiumWebBrowser1.Dock = DockStyle.Fill;
+            this.Controls.Add(_chromiumWebBrowser1);
             this.Shown += Form1_Shown;
         }
 
         private void Form1_Shown(object sender, EventArgs e)
         {
-            chromiumWebBrowser1.Load(URL);
+            _chromiumWebBrowser1.Load(URL);
         }
 
         public string URL { get; internal set; } = "google.com";
